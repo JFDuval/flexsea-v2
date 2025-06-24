fbc234925a6d3e18a002d65bbe078d717f1e7e0a
JFDuval <jfduval@aqra.ca>
2025-06-24 15:26:11 -0400

    Stress Test has been modernized. It's not fully leveraging the latest stack, but it's closer. Should be cross-platform...

--- demo/pc_python/flexsea_stress_test.py
+++ demo/pc_python/flexsea_stress_test.py
@@ -1,6 +1,5 @@
 import ctypes
 from ctypes import *
-import serial
 import time
 import sys
 import datetime
@@ -14,9 +13,19 @@ from flexsea_python import FlexSEAPython
 from flexsea_tools import *
 # Note: with PyCharm you must add this folder and mark is as a Sources Folder to avoid an Unresolved Reference issue
 
-dll_filename = '../../projects/eclipse_pc/DynamicLib/libflexsea-v2.dll'
-com_port = 'COM12'
-serial_port = 0  # Holds the serial port object
+# Platform specific shared library and serial port
+pf = FlexSEAPython.identify_platform()
+if pf == 'WIN':
+    dll_filename = '../../projects/eclipse_pc/DynamicLib/libflexsea-v2.dll'
+    com_port = 'COM12'
+elif pf == 'MAC':
+    dll_filename = '../../../flexsea-v2/projects/eclipse_pc/DynamicLib/libflexsea-v2.dylib'
+    com_port = '/dev/tty.usbserial-ABCD'  # Default, can be over-ridden by CLI argument
+else:
+    # ToDo this is likely too generic
+    dll_filename = '../../../flexsea-v2/projects/eclipse_pc/DynamicLib/libflexsea-v2.so'
+    com_port = '/dev/ttyAMA0'  # Default, can be over-ridden by CLI argument
+
 new_tx_delay_ms = 20  # 10 ms = 100 Hz
 
 MIN_OVERHEAD = 4
@@ -61,26 +70,6 @@ class StressTestData:
 
 
 stress_test_data = []
-
-
-# Open serial port
-def serial_open(com_port_name):
-    global serial_port
-    serial_port = serial.Serial(com_port_name, baudrate=115200)
-    if serial_port:
-        print(f'Successfully opened {com_port_name}.')
-        return 0
-    else:
-        print(f'Could not open {com_port_name}!')
-        return 1
-
-
-# Write to a previously opened serial port
-def serial_write(bytestream, bytestream_length):
-    if serial_port:
-        serial_port.write(bytestream[0:bytestream_length])
-    else:
-        print("No serial port object, can't write!")
 
 
 # C-style data structure
@@ -169,28 +158,6 @@ def counter_and_ramp(cnt, rmp):
     return cnt, rmp
 
 
-# This replicates the embedded system's fx_receive command
-def fx_receive(fx):
-    send_reply = 0
-    cmd_reply = 0
-    new_data = 0
-    if fx.get_circular_buffer_length() > MIN_OVERHEAD:
-        ret_val, cmd_6bits_out, rw_out, buf, buf_len = fx.get_cmd_handler_from_bytestream()
-        if not ret_val:
-            # Call handler:
-            fx.call_cmd_handler(cmd_6bits_out, rw_out, buf)
-            new_data = 1
-            # Reply if requested
-            if (rw_out == fx.rw_dict['CmdRead']) or (rw_out == fx.rw_dict['CmdReadWrite']):
-                reply_cmd = cmd_6bits_out
-                send_reply = 1
-            fx.cleanup()
-    else:
-        fx.cleanup()
-
-    return send_reply, cmd_reply, new_data
-
-
 # Loopback demo: we create a bytestream, shuffle it around, then decode it
 # No serial port required, no interaction with any other system: pure software loopback
 def flexsea_stress_test_local_loopback():
@@ -226,7 +193,7 @@ def flexsea_stress_test_local_loopback():
         fx_dut.write_to_circular_buffer(bytestream, bytestream_len)
 
         # At this point our encoded command is in the DUT's circular buffer. Can we decode it?
-        send_reply, cmd_reply = fx_receive(fx_dut)
+        send_reply, cmd_reply, new_data = fx_dut.receive()
 
         # Do we have a reply to send?
         # (this replicates fx_transmit())
@@ -243,7 +210,7 @@ def flexsea_stress_test_local_loopback():
             fx_pc.write_to_circular_buffer(bytestream, bytestream_len)
 
         # Final step, PC reception
-        fx_receive(fx_pc)
+        fx_pc.receive()
 
     print(f'Packets sent: {pc_packet_number + 1}')
     print(f'Packets received: {len(stress_test_data)}')
@@ -261,13 +228,10 @@ def flexsea_stress_test_serial():
     print(f'This test will take approximately {STRESS_TEST_CYCLES * new_tx_delay_ms / 1000:0.2f} s to run.\n')
 
     # Initialize FlexSEA comm
-    fx = FlexSEAPython(dll_filename)
-
-    # Configure serial port
-    ret_val = serial_open(com_port)
-    if ret_val:
-        print(f"Problem opening the {com_port} serial port - quit.")
-        exit()
+    fx = FlexSEAPython(dll_filename, com_port)
+    fx.who_am_i()
+    if fx.get_pf() == 'RPI':
+        fx.configure_rpi_rs485()
 
     # Prepare for reception:
     fx.register_cmd_handler(FX_CMD_STRESS_TEST, fx_rx_cmd_handler_2_pc)
@@ -278,16 +242,12 @@ def flexsea_stress_test_serial():
     start_time = round(time.time() * 1000)
     bytes_received = 0
 
-    # Flush any old RX and TX bytes
-    serial_port.reset_input_buffer()
-    serial_port.reset_output_buffer()
-
     # Reset embedded counters
     ret_val, bytestream, bytestream_len = fx.create_bytestream_from_cmd(cmd=FX_CMD_STRESS_TEST,
                                                                         rw="CmdWrite",
                                                                         payload_string=gen_stress_test_payload(
                                                                             0, 0, reset=1))
-    serial_write(bytestream, bytestream_len)
+    fx.serial_write(bytestream, bytestream_len)
     time.sleep(0.01)
 
     for i in range(STRESS_TEST_CYCLES):
@@ -300,7 +260,7 @@ def flexsea_stress_test_serial():
                                                                               pc_packet_number, pc_ramp_value))
 
         # Send bytestream to serial port
-        serial_write(bytestream, bytestream_len)
+        fx.serial_write(bytestream, bytestream_len)
         current_time = round(time.time() * 1000)
         tx_timestamp = current_time - start_time
         send_new_tx_cmd_timestamp = current_time + new_tx_delay_ms
@@ -312,11 +272,10 @@ def flexsea_stress_test_serial():
                 current_time = round(time.time() * 1000)
 
                 # Feed any received bytes into the circular buffer
-                bytes_to_read = serial_port.in_waiting
+                bytes_to_read = fx.serial_bytes_available()
                 if bytes_to_read > 0:
-                    # print(f'Bytes to read: {bytes_to_read}.')
                     for i in range(bytes_to_read):
-                        new_rx_byte = serial_port.read(1)
+                        new_rx_byte = fx.serial_read_byte()
                         ret_val = fx.write_to_circular_buffer(new_rx_byte[0], 1)
                         bytes_received = bytes_received + 1
                         if ret_val:
@@ -324,7 +283,7 @@ def flexsea_stress_test_serial():
                             exit()
 
                 # Final step, PC reception
-                fx_receive(fx)
+                fx.receive()
 
         except KeyboardInterrupt:
             print('Interrupted! End of stress test code.')
@@ -338,11 +297,10 @@ def flexsea_stress_test_serial():
         current_time = round(time.time() * 1000)
 
         # Feed any received bytes into the circular buffer
-        bytes_to_read = serial_port.in_waiting
+        bytes_to_read = fx.serial_bytes_available()
         if bytes_to_read > 0:
-            # print(f'Bytes to read: {bytes_to_read}.')
             for i in range(bytes_to_read):
-                new_rx_byte = serial_port.read(1)
+                new_rx_byte = fx.serial_read_byte()
                 ret_val = fx.write_to_circular_buffer(new_rx_byte[0], 1)
                 bytes_received = bytes_received + 1
                 if ret_val:
@@ -350,7 +308,7 @@ def flexsea_stress_test_serial():
                     exit()
 
         # Final step, PC reception
-        fx_receive(fx)
+        fx.receive()
 
     print(f'Packets sent: {pc_packet_number + 1}')
     print(f'Packets received: {len(stress_test_data)} (or {stress_test_data[-1].rx_packet_num})')
@@ -370,6 +328,6 @@ if __name__ == "__main__":
     csv = Csv(f'logs/stress-test-{file_timestamp}.csv', csv_header)
 
     # Available demos, select one or more:
-    # flexsea_stress_test_local_loopback()
-    # print('\n=-=-=-=-=\n')
+    flexsea_stress_test_local_loopback()
+    print('\n=-=-=-=-=\n')
     flexsea_stress_test_serial()
